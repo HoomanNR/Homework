b18e85e64890aae37e170acdf71bf4c98c0f1d62
Hooman <111180271+HoomanNR@users.noreply.github.com>
2022-09-07 01:10:44 +0430

    Delete Money.py

--- Money.py
+++
@@ -1,5 +0,0 @@
-Hours = input("How many hours did you work ? ")
-Hours = int(Hours)
-Salary = input("How much money do you get every hour ? ")
-Salary = float(Salary)
-print("You Worked :" , Hours*Salary)
